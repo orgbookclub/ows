71de05f781a184321b1a75a067fb8b1f1c805d4d
Ravsimar Sodhi <ravsimar@gmail.com>
2022-08-05 21:06:29 +0530

    fix linting for db repositories

--- src/repositories/event.repository.ts
+++ src/repositories/event.repository.ts
@@ -2,63 +2,82 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 
 import { CreateEventDto } from "../events/dto/create-event.dto";
+import { UpdateEventDto } from "../events/dto/update-event.dto";
 import { Event, EventDocument } from "../events/schemas/event.schema";
 
 import { BaseRepository } from "./base.repository";
 
 /**
- *
+ * Repository for handling all DB operations related to @see Event objects.
  */
-export class EventRepository extends BaseRepository<Event> {
+export class EventRepository extends BaseRepository<EventDocument> {
   /**
+   * Initializes an instance of EventRepository.
    *
-   * @param eventModel
+   * @param {Model<EventDocument>} eventModel The mongoose model.
    */
   constructor(
     @InjectModel(Event.name) private readonly eventModel: Model<EventDocument>,
   ) {
     super();
   }
+
   /**
+   * Creates an event document in the database.
    *
-   * @param item
+   * @param {CreateEventDto} item The dto object.
+   * @returns {Promise<EventDocument>} The result document.
    */
-  async create(item: CreateEventDto) {
+  async create(item: CreateEventDto): Promise<EventDocument> {
     return await this.eventModel.create(item);
   }
+
   /**
+   * Gets an event document with the given ID.
    *
-   * @param id
+   * @param {string} id The object ID.
+   * @returns {Promise<EventDocument>} The result document.
    */
-  async get(id: string) {
+  async get(id: string): Promise<EventDocument> {
     return await this.eventModel.findById(id);
   }
+
   /**
+   * Gets all event documents from the DB.
    *
+   * @returns {Promise<EventDocument[]>} The result document list.
    */
-  async getAll() {
+  async getAll(): Promise<EventDocument[]> {
     return await this.eventModel.find().exec();
   }
+
   /**
+   * Gets all event documents which match the query.
    *
-   * @param query
+   * @param {any} query The query object.
+   * @returns {Promise<EventDocument[]>} The result document list.
    */
-  async find(query: any) {
+  async find(query: any): Promise<EventDocument[]> {
     return await this.eventModel.find(query).exec();
   }
+
   /**
+   * Updates the event document in the DB.
    *
-   * @param id
-   * @param updateDto
+   * @param {string} id The object ID of the doc to update.
+   * @param {UpdateEventDto} updateDto The updated doc.
+   * @returns {Promise<EventDocument>} The result document (after update).
    */
-  async update(id: string, updateDto) {
+  async update(id: string, updateDto: UpdateEventDto): Promise<EventDocument> {
     return await this.eventModel.findByIdAndUpdate(id, updateDto, {
       returnDocument: "after",
     });
   }
+
   /**
+   * Deletes a event document from the DB.
    *
-   * @param id
+   * @param {string} id The object ID of the doc.
    */
   async delete(id: string) {
     return await this.eventModel.findByIdAndRemove(id);
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -2,63 +2,82 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 
 import { CreateUserDto } from "../users/dto/create-user.dto";
+import { UpdateUserDto } from "../users/dto/update-user.dto";
 import { User, UserDocument } from "../users/schemas/user.schema";
 
 import { BaseRepository } from "./base.repository";
 
 /**
- *
+ * Repository for handling all DB operations related to @see User objects.
  */
-export class UserRepository extends BaseRepository<User> {
+export class UserRepository extends BaseRepository<UserDocument> {
   /**
+   * Initializes an instance of UserRepository.
    *
-   * @param userModel
+   * @param {Model<UserDocument>} userModel The mongoose model.
    */
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
   ) {
     super();
   }
+
   /**
+   * Creates a user document in the database.
    *
-   * @param item
+   * @param {CreateUserDto} item The dto object.
+   * @returns {Promise<UserDocument>} The result document.
    */
-  async create(item: CreateUserDto) {
+  async create(item: CreateUserDto): Promise<UserDocument> {
     return await this.userModel.create(item);
   }
+
   /**
+   * Gets a user document with the given ID.
    *
-   * @param id
+   * @param {string} id The object ID.
+   * @returns {Promise<UserDocument>} The result document.
    */
-  async get(id: string) {
+  async get(id: string): Promise<UserDocument> {
     return await this.userModel.findById(id).exec();
   }
+
   /**
+   * Gets all user documents from the DB.
    *
+   * @returns {Promise<UserDocument[]>} The result document list.
    */
-  async getAll() {
+  async getAll(): Promise<UserDocument[]> {
     return await this.userModel.find().exec();
   }
+
   /**
+   * Gets all user documents which match the query.
    *
-   * @param query
+   * @param {any} query The query object.
+   * @returns {Promise<UserDocument[]>} The result document list.
    */
-  async find(query: any) {
+  async find(query: any): Promise<UserDocument[]> {
     return await this.userModel.find(query).exec();
   }
+
   /**
+   * Updates the user document in the DB.
    *
-   * @param id
-   * @param updateDto
+   * @param {string} id The object ID of the doc to update.
+   * @param {UpdateUserDto} updateDto The updated doc.
+   * @returns {Promise<UserDocument>} The result document (after update).
    */
-  async update(id: string, updateDto) {
+  async update(id: string, updateDto: UpdateUserDto): Promise<UserDocument> {
     return await this.userModel.findByIdAndUpdate(id, updateDto, {
       returnDocument: "after",
     });
   }
+
   /**
+   * Deletes a user document from the DB.
    *
-   * @param id
+   * @param {string} id The object ID of the doc.
    */
   async delete(id: string) {
     return await this.userModel.findByIdAndRemove(id);
