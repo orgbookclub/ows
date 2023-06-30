e6e6f3e6f500ccc696e4ce2548906bfb3a5d5ccc
Ravsimar Sodhi <>
2023-06-30 23:21:58 +0530

    add port option

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
   Logger.debug("Updated openapi.json");
   SwaggerModule.setup("api", app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
