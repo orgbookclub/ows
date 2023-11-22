92a32d01f8068e60ed60b988f59a0561e108380c
Rav <ravsimar@gmail.com>
2023-11-22 21:20:26 +0530

    Add Scheduled Tasks for Events (#360)
    * throw error in gr controller
    
    * add scheduled tasks
    
    * typo
    
    ---------
    
    Co-authored-by: Ravsimar Sodhi <>

--- src/book-info/goodreads.controller.ts
+++ src/book-info/goodreads.controller.ts
@@ -40,7 +40,11 @@ export class GoodreadsController {
     type: [BookDto],
   })
   async searchBooks(@Query("q") query: string, @Query("k") k: number) {
-    return await this.goodreadsService.searchBooks(query, k);
+    const bookList = await this.goodreadsService.searchBooks(query, k);
+    if (bookList.length === 0) {
+      throw new NotFoundException("Could not find a book by that query");
+    }
+    return bookList;
   }
 
   /**
